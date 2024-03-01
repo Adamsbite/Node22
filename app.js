9e5536db5b83bc71390224bca0f0f3701ad655e8
bright adams <btadamz@gmail.com>
2024-03-01 16:42:32 +0100

    Resound message

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // app.js
 
-const http = re quire('http')
+const http = require('http')
  const server = http.createServer((req, res) => {
-  console.log(req)
-  res.write('welcome to our home you fuck boy page')
+  // console.log(req)
+  res.write('welcome yellow tooooo our home you fuck boy page')
   res.end ()
  })
  server.listen(5000)
+  console.log ( "yesterday is my day")
